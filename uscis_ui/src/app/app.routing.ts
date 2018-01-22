56e7c75a37be148708a0306c9769a890c47521c9
unknown <sivakalyani.tarigopula@gmail.com>
2018-01-21 18:32:36 -0600

    Intial Commit for the UI project

---
+++ uscis_ui/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { UploadComponent } from './upload/index';
+import { AuthGuard } from './_guards/index';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'upload', component: UploadComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
---
+++ uscis_ui/src/app/upload/upload.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, ElementRef, VERSION, Renderer2, ViewChild, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService, AuthenticationService } from '../_services/index';
+import '../../assets/app.css';
+
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'app',
+    templateUrl: 'upload.component.html'
+})
+
+export class UploadComponent implements OnInit{
+    model: any = {};
+    name : string;
+    message: string;
+   
+
+    ngOnInit(){
+    console.log(this.model.uniquecode);
+    }
+    @ViewChild('fileInput') el:ElementRef;
+
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private rd: Renderer2) {
+        this.name = `Angular v${VERSION.full} : File Upload`
+      }
+      
+      fileUpload() {
+        console.log(this.el);
+        console.log(this.model.uniquecode);
+
+        this.message = "File uploaded is :"+this.el.nativeElement.files[0].name;
+        console.log(this.message);
+      }
+
+      backToLogin(){
+          console.log('test back login');
+        this.router.navigate(['login']);
+      }
+
+
+   
+}
